5f640cf0eadb048daeb40407347fe6742107e5e0
rapha1201 <rapha1201@pc-noter>
2024-12-07 14:24:21 -0300

    Adicionando 3 testes E2E em spec.cy.js - TP2 ES

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -69,3 +69,129 @@ describe('TODOMvc App', () => {
       .should('have.length', 2);
   });
 });
+
+it('Insere 3 tarefas e marca a segunda como concluída, depois filtra por completas e exclui a segunda tarefa.', () => {
+  cy.visit(''); 
+
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1 - Teste{enter}')
+    .type('Tarefa 2 - Teste{enter}')
+    .type('Tarefa 3 - Teste{enter}');
+
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .eq(1)
+    .click();
+  
+  cy.get('[data-cy=filter-active-link]')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 2)
+    .eq(0)
+    .should('have.text', 'Tarefa 1 - Teste');
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .eq(1)
+    .should('have.text', 'Tarefa 3 - Teste');
+
+  cy.get('[data-cy=filter-completed-link]')
+    .click()
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1)
+    .eq(0)
+    .should('have.text', 'Tarefa 2 - Teste');
+
+  cy.get('[data-cy=todos-list] > li [data-cy=remove-todo-btn]')
+    .first()
+    .invoke('show')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 0);
+    
+});
+
+it('Insere 2 tarefas e marca todas como concluídas.', () => {
+  cy.visit(''); 
+
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1 - Teste{enter}')
+    .type('Tarefa 2 - Teste{enter}');
+
+  cy.get('.toggle-all-label')
+    .click();
+
+  cy.get('[data-cy=filter-active-link]')
+    .click();
+  
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 0);
+
+  cy.get('[data-cy=filter-completed-link]')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 2);
+    
+});
+
+it('Insere 3 tarefas, marca todas como concluídas, remove todas as concluídas, adiciona uma nova tarefa e marca como concluída.', () => {
+
+  cy.visit('');
+
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 1 - T1{enter}')
+    .type('Tarefa 2 - T2{enter}')
+    .type('Tarefa 3 - T3{enter}');
+  
+  cy.get('.toggle-all-label')
+    .click();
+
+  cy.get('[data-cy=filter-completed-link]')
+    .click();
+  
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 3);
+
+  cy.get('[data-cy=todo-input]')
+    .type('Tarefa 4 - T4{enter}');
+
+  cy.get('.clear-completed')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 0);
+
+  cy.get('[data-cy=filter-active-link]')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1);
+
+  cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+    .first()
+    .invoke('show')
+    .click();
+  
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 0); 
+  
+  cy.get('[data-cy=filter-completed-link]')
+    .click();
+
+  cy.get('[data-cy=todos-list]')
+    .children()
+    .should('have.length', 1);
+
+})
